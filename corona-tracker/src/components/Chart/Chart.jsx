18a23cd0eda32109773676fe7cc08c4c0915ee06
troyfrncs <frncsgelay@gmail.com>
2021-03-29 22:24:38 +0800

    test push updated

--- corona-tracker/src/components/Chart/Chart.jsx
+++ corona-tracker/src/components/Chart/Chart.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { fetchDailyData } from '../../api';
 import { Line, Bar } from 'react-chartjs-2';
 
+
+
 import styles from './Chart.module.css';
 
 const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
